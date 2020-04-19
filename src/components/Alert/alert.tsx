39414b28ac477c803333d6b236a1fd5524395f5b
Yunwen Zheng <ynn.zheng@gmail.com>
2020-04-18 23:46:15 -0400

    updated alert component

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -15,42 +15,26 @@ export interface BaseAlertProps {
     message: string;
     description?: string;
     closable?: boolean;
-}
-
-interface BaseAlertButtonProps {
-    children: React.ReactNode;
-    className?: string;
-}
-
-type NativeButtonProps = BaseAlertButtonProps & React.ButtonHTMLAttributes<HTMLElement>
-type AnchorButtonProps = BaseAlertButtonProps & React.AnchorHTMLAttributes<HTMLElement>
-export type AlertCloseButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
-
-export const AlertCloseButton: React.FC<AlertCloseButtonProps> = (props) => {
-
-    const { children, className, ...restProps } = props
-
-    return(
-        <button
-            data-testid='close-btn'
-            className={className}
-            {...restProps}
-        >
-            {children}
-        </button>
-    )
+    onClose?: () => void;
 }
 
 
 const Alert: React.FC<BaseAlertProps> = (props) => {
     const [show, setShow] = useState(true);
 
-    const { className, alertType, message, description, closable } = props;
+    const { className, alertType, message, description, closable, onClose } = props;
 
     const classes = classNames('alert', className, {
         [`alert-${alertType}`]: alertType,
         'closable': (closable === true) && closable
     })
+
+    const handleClose = (e: React.MouseEvent) => {
+        if (onClose) {
+          onClose()
+        }
+        setShow(!show)
+      }
 
     if(show) {
         return(
@@ -63,19 +47,19 @@ const Alert: React.FC<BaseAlertProps> = (props) => {
                     { description && <div className='description'>{description}</div>}
                 </div>
                 { closable === true && 
-                    <AlertCloseButton
+                    <span
                         data-testid='close-btn'
                         className='close-btn'
-                        onClick={() => setShow(false)}
+                        onClick={handleClose}
                     >
                         x
-                    </AlertCloseButton>
+                    </span>
                 }
             </div>
         )
     }
     return(
-        <Button onClick={() => setShow(true)}>Show Alert</Button>
+        <Button onClick={handleClose}>Show Alert</Button>
     )
 }
 
