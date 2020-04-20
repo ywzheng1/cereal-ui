aa5ad3c33a7cac0dee21f9e59d54c531e40b1d9a
Yunwen Zheng <ynn.zheng@gmail.com>
2020-04-19 23:07:17 -0400

    updated alert component unit test

--- src/components/Alert/alert.test.tsx
+++ src/components/Alert/alert.test.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import Alert, { AlertType, BaseAlertProps, AlertCloseButton, AlertCloseButtonProps } from './alert'
+import Alert, { AlertType, BaseAlertProps } from './alert'
 
 const defaultProps: BaseAlertProps = {
     message: 'alert message',
     alertType: AlertType.Default,
+    closable: true,
+    onClose: jest.fn()
 }
-
-const buttonProps:AlertCloseButtonProps = {
-    onClick: jest.fn(),
-}
-
 
 describe('test Alert component', () => {
     it('should render the correct default alert component', () => {
@@ -18,13 +15,16 @@ describe('test Alert component', () => {
         const element = wrapper.getByTestId('alert')
         expect(element).toBeInTheDocument()
         expect(element).toHaveClass('alert alert-default')
+        const btnElement = wrapper.getByTestId('close-btn')
+        fireEvent.click(btnElement)
+        expect(defaultProps.onClose).toHaveBeenCalled()
     })
 
-    it('should close alert box when click on "x" button', () => {
-        const wrapper = render(<AlertCloseButton {...buttonProps}>x</AlertCloseButton>)
-        const element = wrapper.getByText('x')
-        expect(element).toBeInTheDocument()
-        fireEvent.click(element)
-        expect(buttonProps.onClick).toHaveBeenCalled()
-    })
+    // it('should close alert box when click on "x" button', () => {
+    //     const wrapper = render(<AlertCloseButton {...buttonProps}>x</AlertCloseButton>)
+    //     const element = wrapper.getByText('x')
+    //     expect(element).toBeInTheDocument()
+    //     fireEvent.click(element)
+    //     expect(buttonProps.onClick).toHaveBeenCalled()
+    // })
 })
