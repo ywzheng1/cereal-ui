ffc900c47d3bc6cb47f0982a810a2c6f99c035cd
Yunwen Zheng <ynn.zheng@gmail.com>
2020-04-22 14:00:52 -0400

    added Transition component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import React, { useState } from 'react';
+import Button from './components/Button/button';
 import Alert, { AlertType } from './components/Alert/alert';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
@@ -9,85 +9,71 @@ import TabItem from './components/Tab/tabItem';
 import Icon from './components/Icon/icon';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import Transition from './components/Transition/transition'
+
 import './App.scss';
 
 library.add(fas)
 
 function App() {
 
-  return (
-	<div className='app'>
-		<div className='buttons'>
-			<h1>Buttons</h1>
-			<div className='button-group'>
-				<Button btnType={ButtonType.Primary}> Primary </Button>
-				<Button> Normal </Button>
-				<Button disabled> Disabled </Button>
-				<Button btnType={ButtonType.Danger}> Danger </Button>
-				<Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Button </Button>
-				<Button btnType={ButtonType.Primary} size={ButtonSize.Small}> Small </Button>
-				<Button btnType={ButtonType.Link} href="https://www.google.com/"> Link Button </Button>
-				<Button btnType={ButtonType.Link} href="https://www.google.com/" disabled> Disabled Link </Button>
+	const [ show, setShow ] = useState(false)
+
+	return (
+		<div className='app'>
+			<div className='buttons'>
+				<h1>Buttons</h1>
+				<div className='button-group'>
+					<Button btnType='primary'> Primary </Button>
+					<Button> Normal </Button>
+					<Button disabled> Disabled </Button>
+					<Button btnType='danger'> Danger </Button>
+					<Button btnType='primary' size='lg'> Large Button </Button>
+					<Button btnType='primary' size='sm'> Small </Button>
+					<Button btnType='link' href="https://www.google.com/"> Link Button </Button>
+					<Button btnType='link' href="https://www.google.com/" disabled> Disabled Link </Button>
+				</div>
 			</div>
-		</div>
-		<hr />
-		<div className='alerts'>
-			<h1>Alerts</h1>
-			<div className='alert-group'>
-				<Alert 
-					alertType={AlertType.Default} 
-					message="this is alert message"
-					closable={true}
-				/>
+			<hr />
+			<div className='alerts'>
+				<h1>Alerts</h1>
+				<div className='alert-group'>
+					<Alert 
+						alertType={AlertType.Default} 
+						message="this is alert message"
+						closable={true}
+					/>
 
-				<Alert 
-					alertType={AlertType.Success} 
-					message="this is alert message"
-					closable={true}
-				/>
+					<Alert 
+						alertType={AlertType.Success} 
+						message="this is alert message"
+						closable={true}
+					/>
 
-				<Alert 
-					alertType={AlertType.Warning} 
-					message="this is alert message"
-					closable={true}
-				/>
+					<Alert 
+						alertType={AlertType.Warning} 
+						message="this is alert message"
+						closable={true}
+					/>
 
-				<Alert 
-					alertType={AlertType.Danger} 
-					message="this is alert message"
-					description='this is a description'
-					closable={true}
-				/>
+					<Alert 
+						alertType={AlertType.Danger} 
+						message="this is alert message"
+						description='this is a description'
+						closable={true}
+					/>
+				</div>
 			</div>
-		</div>
-		<hr/>
-		<div className='menus'>
-			<h1>Menus</h1>
-			<Menu defaultIndex='0' onSelect={(index) => alert(index)}>
-				<MenuItem>cool link</MenuItem>
-				<MenuItem disabled>cool link 2</MenuItem>
-				<MenuItem>cool link 3</MenuItem>
-			</Menu>
-
-			<Menu defaultIndex='0' onSelect={(index) => alert(index)}>
-				<MenuItem>cool link</MenuItem>
-				<MenuItem disabled>cool link 2</MenuItem>
-				<SubMenu title='dropdown'>
-					<MenuItem>dropdown 1</MenuItem>
-					<MenuItem>dropdown 2</MenuItem>
-					<MenuItem>dropdown 3</MenuItem>
-				</SubMenu>
-				<MenuItem>cool link 3</MenuItem>
-			</Menu>
-
-			<div className='vertical-menus-group'>
-				<Menu defaultIndex='0' mode="vertical" onSelect={(index) => alert(index)}>
+			<hr/>
+			<div className='menus'>
+				<h1>Menus</h1>
+				<Menu defaultIndex='0' onSelect={(index) => alert(index)}>
 					<MenuItem>cool link</MenuItem>
 					<MenuItem disabled>cool link 2</MenuItem>
 					<MenuItem>cool link 3</MenuItem>
 				</Menu>
-				
-				<Menu defaultIndex='0' mode='vertical' onSelect={(index) => alert(index)}>
+
+				<Menu defaultIndex='0' onSelect={(index) => alert(index)}>
 					<MenuItem>cool link</MenuItem>
 					<MenuItem disabled>cool link 2</MenuItem>
 					<SubMenu title='dropdown'>
@@ -98,39 +84,80 @@ function App() {
 					<MenuItem>cool link 3</MenuItem>
 				</Menu>
 
-				<Menu defaultIndex='0' mode='vertical' onSelect={(index) => alert(index)} defaultOpenSubMenus={['2']}>
-					<MenuItem>cool link</MenuItem>
-					<MenuItem disabled>cool link 2</MenuItem>
-					<SubMenu title='dropdown default open'>
-						<MenuItem>dropdown 1</MenuItem>
-						<MenuItem>dropdown 2</MenuItem>
-						<MenuItem>dropdown 3</MenuItem>
-					</SubMenu>
-					<MenuItem>cool link 3</MenuItem>
-				</Menu>
+				<div className='vertical-menus-group'>
+					<Menu defaultIndex='0' mode="vertical" onSelect={(index) => alert(index)}>
+						<MenuItem>cool link</MenuItem>
+						<MenuItem disabled>cool link 2</MenuItem>
+						<MenuItem>cool link 3</MenuItem>
+					</Menu>
+					
+					<Menu defaultIndex='0' mode='vertical' onSelect={(index) => alert(index)}>
+						<MenuItem>cool link</MenuItem>
+						<MenuItem disabled>cool link 2</MenuItem>
+						<SubMenu title='dropdown'>
+							<MenuItem>dropdown 1</MenuItem>
+							<MenuItem>dropdown 2</MenuItem>
+							<MenuItem>dropdown 3</MenuItem>
+						</SubMenu>
+						<MenuItem>cool link 3</MenuItem>
+					</Menu>
+
+					<Menu defaultIndex='0' mode='vertical' onSelect={(index) => alert(index)} defaultOpenSubMenus={['2']}>
+						<MenuItem>cool link</MenuItem>
+						<MenuItem disabled>cool link 2</MenuItem>
+						<SubMenu title='dropdown default open'>
+							<MenuItem>dropdown 1</MenuItem>
+							<MenuItem>dropdown 2</MenuItem>
+							<MenuItem>dropdown 3</MenuItem>
+						</SubMenu>
+						<MenuItem>cool link 3</MenuItem>
+					</Menu>
+				</div>
+			</div>
+			<hr/>
+			<div className='tabs-section'>
+				<h1>Tabs</h1>
+				<div className='tabs-section-inner'>
+					<Tabs defaultIndex={0}>
+						<TabItem label='card1'>this is card one</TabItem>
+						<TabItem label='card2'>this is card two</TabItem>
+						<TabItem label='card3'>this is card three</TabItem>
+					</Tabs>
+
+					<Tabs defaultIndex={0} type='card'>
+						<TabItem label='card1'>this is card one</TabItem>
+						<TabItem label='card2'>this is card two</TabItem>
+						<TabItem label='card3' disabled={true}>this is card three</TabItem>
+					</Tabs>
+				</div>
+			</div>
+			<hr/>
+			<div className='icons-section'>
+				<h1>Icons</h1>
+				<Icon icon='coffee' theme='danger' size='2x'/>
+				<Icon icon='arrow-down' theme='primary' size='2x'/>
+				<Icon icon='book' theme='info' size='2x'/>
+			</div>
+			<hr/>
+			<div>
+				<h1>Transition</h1>
+				<Button size='sm' onClick={() => {setShow(!show)}}>Toggle</Button>
+				<Transition
+					in={show}
+					timeout={300}
+					animation='zoom-in-left'
+				>
+					<div>
+						<p>Some content</p>
+						<p>Some content</p>
+						<p>Some content</p>
+						<p>Some content</p>
+						<p>Some content</p>
+					</div>
+				</Transition>
 			</div>
 		</div>
-		<hr/>
-		<div className='tabs-section'>
-			<Tabs defaultIndex={0}>
-				<TabItem label='card1'>this is card one</TabItem>
-				<TabItem label='card2'>this is card two</TabItem>
-				<TabItem label='card3'>this is card three</TabItem>
-			</Tabs>
-
-			<Tabs defaultIndex={0} type='card'>
-				<TabItem label='card1'>this is card one</TabItem>
-				<TabItem label='card2'>this is card two</TabItem>
-				<TabItem label='card3' disabled={true}>this is card three</TabItem>
-			</Tabs>
-		</div>
-		<div className='icons-section'>
-			<Icon icon='coffee' theme='danger' size='2x'/>
-			<Icon icon='arrow-down' theme='primary' size='2x'/>
-			<Icon icon='book' theme='info' size='2x'/>
-		</div>
-	</div>
-  );
+	);
 }
 
 export default App;
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useContext, FunctionComponentElement } from 'react'
 import classNames from 'classnames'
-import { CSSTransition } from 'react-transition-group'
 import { MenuContext } from './menu'
 import { MenuItemProps } from './menuItem'
 import Icon from '../Icon/icon';
+import Transition from '../Transition/transition'
 
 export interface SubMenuProps {
     index?: string;
@@ -60,17 +60,15 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className }) 
             }
         })
         return (
-            <CSSTransition
+            <Transition
                 in={menuOpen}
                 timeout={300}
-                classNames="zoom-in-top"
-                appear
-                unmountOnExit
+                animation="zoom-in-top"
             >
                 <ul className={subMenuClasses}>
                     {childrenComponent}
                 </ul>
-            </CSSTransition>
+            </Transition>
         )
     }
 
