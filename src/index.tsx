f989e3d07decc867cdcf72d4de3711ebb9e243dc
Yunwen Zheng <ynn.zheng@gmail.com>
2020-05-06 16:45:58 -0400

    fixed import error in idnex.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,22 +12,3 @@ export { default as Transition } from './components/Transition'
 export { default as Upload } from './components/Upload'
 export { default as Tabs } from './components/Tab'
 export { default as Alert } from './components/Alert'
-export { default as Select } from './components/Select'
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './styles/index.scss';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
