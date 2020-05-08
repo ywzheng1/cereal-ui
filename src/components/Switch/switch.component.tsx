4b0330e4b86e84a000e26950bf8964de4f36e89e
Yunwen Zheng <ynn.zheng@gmail.com>
2020-05-08 18:06:00 -0400

    added sizes and styles to swtich component

--- src/components/Switch/switch.component.tsx
+++ src/components/Switch/switch.component.tsx
@@ -1,25 +1,37 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, FormEvent } from 'react'
 import classNames from 'classnames';
 
-export type SwitchSize = 'lg' | 'sm'
+export type SwitchSize = 'default' | 'sm'
 
 export interface SwitchProps {
+    /** additional class to Switch */
+    className?: string;
+    /** determine whether the Switch is checked */
     checked?: boolean;
+    /** content to be shown when the state is checked */
+    checkedChildren?: string | React.ReactNode;
+    /** content to be shown when the state is unchecked */
+    unCheckedChildren?: string | React.ReactNode;
+    /** Disable switch */
     disabled?: boolean;
+    /**  the size of the Switch, options: default | sm */
     size?: SwitchSize;
-    onChange?: (check:boolean) => void;
+    /** trigger when the checked state is changing */
+    onChange?: (checked:boolean) => void;
 }
 
 export const Switch: FC<SwitchProps> = (props) => {
-    const { checked, disabled, onChange } = props
+    const { checked, className, disabled, onChange, checkedChildren, unCheckedChildren, size } = props
 
     const [ check, setCheck ] = useState(checked ? checked : false)
 
-    const classes = classNames('cereal-switch', {
-        'disabled': disabled
+    const classes = classNames('cereal-switch', className, {
+        'disabled': disabled,
+        'small': size === 'sm'
     })
 
-    const onSwitchChange = (disabled: boolean | undefined) => {
+    const onSwitchChange = (disabled: boolean | undefined, e: FormEvent) => {
+        e.stopPropagation()
         if(!disabled) {
             setCheck(!check)
             if(onChange) {
@@ -29,9 +41,15 @@ export const Switch: FC<SwitchProps> = (props) => {
     }
     
     return(
-        <label className={classes} onChange={() => onSwitchChange(disabled)}>
+        <label className={classes} onChange={(e) => onSwitchChange(disabled, e)} >
             <input type="checkbox" checked={check} />
-            <span className="cereal-slider round"></span>
+            <span className="cereal-slider round">
+                {check 
+                ? 
+                <span className='checked-children'>{checkedChildren}</span> 
+                : <span className='unchecked-children'>{unCheckedChildren}</span>
+                }
+            </span>
         </label>
     )
 }
