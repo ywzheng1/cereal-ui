1157241444bbfb3d3fefd4f4b1f66bb4be17a1aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-01 03:05:38 +0000

    Deploy ywzheng1/cereal-ui to github.com/ywzheng1/cereal-ui.git:gh-pages

---
+++ precache-manifest.c1c08410fc32e9e57c4836fdc926a67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0997c55c9f36c66f68484d4d7c1ce984",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6eb76c208f0b299d13cf.bundle.js"
+  },
+  {
+    "url": "runtime~main.6eb76c208f0b299d13cf.bundle.js"
+  },
+  {
+    "revision": "cdfc47112d02f91d8f81",
+    "url": "static/css/main.1c2daf2d.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.6eb76c208f0b299d13cf.bundle.js"
+  },
+  {
+    "url": "vendors~main.6eb76c208f0b299d13cf.bundle.js.LICENSE.txt"
+  }
+]);
